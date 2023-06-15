9bc24a11f654f28cd69b96998e99e26296069623
Soukaina102 <soukainalotfi102@gmail.com>
2023-06-15 07:27:55 -0700

    new change

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-#!/bin/bash
-#include <stdio.h>
-int main (){
-printf("Programming is like building a multilingual puzzle");
-return 0
+#include<stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: A C program that print with printf ()
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	 puts("Programming is like building a multilingual puzzle");
+	 return (0);
 }
